1b407ae5c1d0f573947e5ce7cde301fe40b08a69
oyvvl <940709@gmail.com>
2020-01-21 20:34:36 +0900

    modified personSchema: added name property

--- server/models/Person.js
+++ server/models/Person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const personSchema = mongoose.Schema({
-
+  name : { type: String, required: true, unique: true }
 })
 
 const Person = mongoose.model('Person', personSchema)
