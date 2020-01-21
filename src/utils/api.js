77cde92209eb24f38022a315909d123d9b497111
oyvvl <940709@gmail.com>
2020-01-22 00:07:48 +0900

    Updated api.js to call fetch people data from the server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,17 @@
-export function fetchPeople () {
+import { SERVER_URL } from './constant';
 
+const handleErrors = response => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
+};
+
+export function fetchPeople() {
+  const url = `${SERVER_URL}/api/people`;
+  const options = {
+    method: 'GET',
+  };
+
+  return fetch(url, options).then(handleErrors);
 }
