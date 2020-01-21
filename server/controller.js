f1dd7ccdd20d798f90d012dd8cde0b90c9572377
oyvvl <940709@gmail.com>
2020-01-22 01:29:21 +0900

    updated controller.js to handle POST request to /api/people

--- server/controller.js
+++ server/controller.js
@@ -5,7 +5,20 @@ const router = express.Router();
 
 router.get('/people', async (req, res) => {
   let people = await Person.find();
-  res.json({people});
+  res.json(people);
 });
 
+router.post('/people', async (req, res) => {
+  const { name } = req.query;
+  const newPerson = await new Person({ name });
+  newPerson.save((err, person) => {
+    if (err) {
+      console.log('console.error', err);
+      return res.status(500).send('Internal Server Error');
+    }
+    console.log(person.name + ' saved to people collection');
+  });
+
+  return res.json(newPerson);
+});
 module.exports = router;
