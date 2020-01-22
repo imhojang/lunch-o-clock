1de0e5b8dde1c2f7d9827e6dc776ac03504345bc
oyvvl <940709@gmail.com>
2020-01-22 14:37:10 +0900

    updated api.js to include POST request to add a person to the list

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,7 @@
 import { SERVER_URL } from './constant';
 
+const baseURL = `${SERVER_URL}/api/people`;
+
 const handleErrors = response => {
   if (!response.ok) {
     throw Error(response.statusText);
@@ -8,10 +10,18 @@ const handleErrors = response => {
 };
 
 export function fetchPeople() {
-  const url = `${SERVER_URL}/api/people`;
   const options = {
     method: 'GET',
   };
+  return fetch(baseURL, options).then(handleErrors);
+}
 
+export function addPerson(name) {
+  const url = new URL(baseURL);
+  const params = { name };
+  url.search = new URLSearchParams(params).toString();
+  const options = {
+    method: 'POST',
+  };
   return fetch(url, options).then(handleErrors);
 }
