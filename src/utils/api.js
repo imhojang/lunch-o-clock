dba639e4fa8f80a42ea253aaaa7bfb0cb8d421b1
oyvvl <940709@gmail.com>
2020-01-23 18:34:08 +0900

    added deletePerson api call to api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,12 @@ import { SERVER_URL } from './constant';
 
 const baseURL = `${SERVER_URL}/api/people`;
 
+const addQueryStringToURL = (params, url) => {
+  const urlObj = new URL(url);
+  urlObj.search =  new URLSearchParams(params).toString();
+  return urlObj
+}
+
 const handleErrors = response => {
   if (!response.ok) {
     throw Error(response.statusText);
@@ -9,19 +15,25 @@ const handleErrors = response => {
   return response.json();
 };
 
-export function fetchPeople() {
+export const fetchPeople = () => {
   const options = {
     method: 'GET',
   };
   return fetch(baseURL, options).then(handleErrors);
 }
 
-export function addPerson(name) {
-  const url = new URL(baseURL);
-  const params = { name };
-  url.search = new URLSearchParams(params).toString();
+export const addPerson = name => {
+  const url = addQueryStringToURL({ name }, baseURL);
   const options = {
     method: 'POST',
+  };
+  return fetch(url, options).then(handleErrors);
+}
+
+export const deletePerson = name => {
+  const url = addQueryStringToURL({ name }, baseURL);
+  const options = {
+    method: 'DELETE',
   };
   return fetch(url, options).then(handleErrors);
 }
