ac38d233c3deb4d9c83d06fda4c8e51b0c31f363
oyvvl <940709@gmail.com>
2020-01-23 18:28:39 +0900

    added action creator, action, reducer to handle delete person

--- src/modules/lunch.js
+++ src/modules/lunch.js
@@ -10,6 +10,9 @@ export const FETCH_PEOPLE_FAILURE = 'FETCH_PEOPLE_FAILURE';
 export const ADD_PERSON_BEGIN = 'ADD_PERSON_BEGIN';
 export const ADD_PERSON_SUCCESS = 'ADD_PERSON_SUCCESS';
 export const ADD_PERSON_FAILURE = 'ADD_PERSON_FAILURE';
+export const DELETE_PERSON_BEGIN = 'DELETE_PERSON_BEGIN';
+export const DELETE_PERSON_SUCCESS = 'DELETE_PERSON_SUCCESS';
+export const DELETE_PERSON_FAILURE = 'DELETE_PERSON_FAILURE';
 
 // ------------------------------------
 // Actions
@@ -30,28 +33,31 @@ export const fetchPeopleFailure = error => ({
 
 export const addPersonBegin = () => ({
   type: ADD_PERSON_BEGIN,
-})
+});
 
 export const addPersonSuccess = person => ({
   type: ADD_PERSON_SUCCESS,
   payload: { person },
-})
+});
 
 export const addPersonFailure = error => ({
   type: ADD_PERSON_FAILURE,
   payload: { error },
-})
+});
 
-export const addPerson = name => {
-  return (dispatch, getState) => {
-    dispatch(addPersonBegin());
-    return api.addPerson(name).then(data => {
-      dispatch(addPersonSuccess(data));
-      return data;
-    })
-    .catch(error => dispatch(addPersonFailure(error)));
-  }
-}
+export const deletePersonBegin = () => ({
+  type: DELETE_PERSON_BEGIN,
+});
+
+export const deletePersonSuccess = person => ({
+  type: DELETE_PERSON_SUCCESS,
+  payload: { person },
+});
+
+export const deletePersonFailure = error => ({
+  type: DELETE_PERSON_FAILURE,
+  payload: { error },
+});
 
 export const fetchPeople = () => {
   return (dispatch, getState) => {
@@ -66,6 +72,32 @@ export const fetchPeople = () => {
   };
 };
 
+export const addPerson = name => {
+  return (dispatch, getState) => {
+    dispatch(addPersonBegin());
+    return api
+      .addPerson(name)
+      .then(data => {
+        dispatch(addPersonSuccess(data));
+        return data;
+      })
+      .catch(error => dispatch(addPersonFailure(error)));
+  };
+};
+
+export const deletePerson = name => {
+  return (dispatch, getState) => {
+    dispatch(deletePersonBegin());
+    return api
+      .deletePerson(name)
+      .then(data => {
+        dispatch(deletePersonSuccess(data));
+        return data;
+      })
+      .catch(error => dispatch(deletePersonFailure(error)));
+  };
+};
+
 export const actions = {};
 
 // ------------------------------------
@@ -76,9 +108,13 @@ const initialState = {
   list: [],
   loading: false,
   error: null,
-}
+};
 
 function people(state = initialState, action) {
+  let list;
+  let person;
+  let updatedList;
+
   switch (action.type) {
     case FETCH_PEOPLE_BEGIN:
       return {
@@ -91,34 +127,55 @@ function people(state = initialState, action) {
         loading: false,
         error: null,
         list: action.payload.people,
-      }
+      };
     case FETCH_PEOPLE_FAILURE:
       return {
         ...state,
         loading: false,
         error: action.payload.error,
-      }
-    case ADD_PERSON_BEGIN: 
+      };
+    case ADD_PERSON_BEGIN:
       return {
         ...state,
         loading: true,
-        error: null
-      }
+        error: null,
+      };
     case ADD_PERSON_SUCCESS:
-      const { list } = state;
-      const { person } = action.payload
-      const updatedList = list.concat(person);
+      list = state.list;
+      person = action.payload.person;
+      updatedList = list.concat(person);
       return {
         list: updatedList,
         loading: false,
         error: null,
-      }
+      };
     case ADD_PERSON_FAILURE:
       return {
         ...state,
         loading: false,
-        error: action.payload.error
-      }
+        error: action.payload.error,
+      };
+    case DELETE_PERSON_BEGIN:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case DELETE_PERSON_SUCCESS:
+      list = state.list;
+      person = action.payload.person;
+      updatedList = list.filter(item => item.name !== person.name);
+      return {
+        list: updatedList,
+        loading: false,
+        error: null,
+      };
+    case DELETE_PERSON_FAILURE:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
     default:
       return state;
   }
