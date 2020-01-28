6126623ec12f8f529fab77cabc61c68e6033d670
oyvvl <940709@gmail.com>
2020-01-29 00:56:55 +0900

    reformatted code

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Input.css';
 
-const Input = (props) => {
-  const placeholderText = 'Type and press Enter key to add to list'
+const Input = ({ value, handleSubmit, handleChange }) => {
+  const placeholderText = 'Type and press Enter key to add to list';
   return (
-    <form className='input-container' onSubmit={props.handleSubmit}>
+    <form className='input-container' onSubmit={handleSubmit}>
       <input
         type='text'
-        value={props.value}
-        onChange={props.handleChange}
+        value={value}
+        onChange={handleChange}
         placeholder={placeholderText}
       />
     </form>
