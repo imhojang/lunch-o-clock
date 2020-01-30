4f794178193b5f882d68acdea8759508db13534a
oyvvl <940709@gmail.com>
2020-01-30 18:16:24 +0900

    added eslint disable line no console

--- server/controller.js
+++ server/controller.js
@@ -29,16 +29,16 @@ router.post('/people', async (req, res) => {
   const people = await Person.find()
 
   if (isDuplicateName(name, people)) {
-    console.log(`The following name "${name}" already exists`)
+    console.log(`The following name "${name}" already exists`) // eslint-disable-line no-console
     res.status(400).send(`${name} already exists`)
   } else {
     const newPerson = await new Person({ name })
     newPerson.save((err, person) => {
       if (err) {
-        console.log('console.error', err.message)
+        console.log('console.error', err.message) // eslint-disable-line no-console
         return res.status(500).send('Internal Server Error')
       } else {
-        console.log(`Successfully saved ${person.name} to people collection`)
+        console.log(`Successfully saved ${person.name} to people collection`) // eslint-disable-line no-console
         return res.json(person)
       }
     })
@@ -49,7 +49,7 @@ router.delete('/people', async (req, res) => {
   const { name } = req.query
   const deletedPerson = await Person.findOneAndDelete({ name })
   if (deletedPerson) {
-    console.log(`Successfully removed ${deletedPerson.name} from people collection`)
+    console.log(`Successfully removed ${deletedPerson.name} from people collection`) // eslint-disable-line no-console
     res.json(deletedPerson)
   } else {
     res.status(400)
